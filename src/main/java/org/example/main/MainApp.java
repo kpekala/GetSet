c53866f8a6758b33d8be90dd52c3dcc52398c112
kpekala <kgpekala@gmail.com>
2022-01-11 21:56:15 +0100

    fix: removed jar files from gitignore and added gradle wrapper

--- src/main/java/org/example/main/MainApp.java
+++ src/main/java/org/example/main/MainApp.java
@@ -2,7 +2,6 @@ package org.example.main;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
